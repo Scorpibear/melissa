ff07f0b1b4c5f03aab43b54ca5daf3201c8a44ab
Mikhail Subach <scorpibear@tut.by>
2014-10-14 21:52:27 +0300

    Initial implementation

---
+++ www/js/shahmaster-tests.js
@@ -0,0 +1,24 @@
+var shahmaster-tests = {
+    "board shows position to solve":                            true,
+    "reset move board to initial position":                     true,
+    "information that answer is correct is shown":              true,
+    "information that move is not the best is displayed":       true,
+    "position pgn is shown":                                    true,
+    "next position button available":                           true,
+    "there is an array of positions and answers":               true,
+    "correct board side is shown":                              true,
+    "next position is always different":                        true,
+    "random selection from array of positions and answers":     true,
+    "pgn is updated after good move":                           true,
+    "when best move is shown, board orientation remains":       true,
+    "snapback on not the best move":                            true,
+    "reset resets hint":                                        true,
+    "mind map is transformed into base object":                 true,
+    "base object is part of the solution":                      true,
+    "base is used for taking positions":                        true,
+    "next level of base is used for taking positions":          true,
+    "only best positions are shown":                            false
+    // next position in a sec after correct answer:             false,
+    // show value after correct answer: false
+
+};
