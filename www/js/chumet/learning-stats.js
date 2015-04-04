a82004775b02c09ac02e88f8815907afc107ba1b
Mikhail Subach <scorpibear@tut.by>
2015-04-04 08:07:34 +0300

    rewrite startpoint

--- www/js/chumet/learning-stats.js
+++
@@ -1,9 +0,0 @@
-function LearningStats(puzzles){
-    this.getPositionsLearnt = function(){
-        var learnt = 0;
-        for(var i=0;i<puzzles.length;i++){
-            if(puzzles[i].isLearnt())
-                learnt++;
-        }
-    }
-}
