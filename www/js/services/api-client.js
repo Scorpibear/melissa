3bee4a52b26644c9341f66fcb432118791f3268b
Mikhail Subach <mikhail_subach@epam.com>
2021-11-05 16:13:14 +0300

    add constant for backendUrl

--- www/js/services/api-client.js
+++ www/js/services/api-client.js
@@ -1,8 +1,12 @@
 angular.module("melissa.services")
-.factory('apiClient', [() => {
+.constant('backendUrl', 'http://umain-02.cloudapp.net:9966')
+.factory('apiClient', ['backendUrl', (backendUrl) => {
   return {
     getFenData: (fen) => {
       return Promise.resolve({bestMove: 'c4'});
+    },
+    updateBase: () => {
+      return Promise.reject('not implemented, the logic to be moved from baseUpdater');
     }
   };
 }])
