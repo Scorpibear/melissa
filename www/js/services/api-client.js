bbcbb4ce39090b83f0bf737ea3ee1830554d362b
Mikhail Subach <mikhail_subach@epam.com>
2021-11-11 19:43:57 +0300

    draft implementatino of apiClient.updateBase

--- www/js/services/api-client.js
+++ www/js/services/api-client.js
@@ -1,13 +1,35 @@
 angular.module("melissa.services")
 .constant('backendUrl', 'http://umain-02.cloudapp.net:9966')
-.factory('apiClient', ['$http', 'backendUrl', ($http, backendUrl) => {
+.factory('apiClient', 
+    ['$http', 'backendUrl', 'userService', 
+    ($http, backendUrl, userService) => {
   return {
     getFenData: (fen) => {
       return $http.get(backendUrl + '/api/fenData', {params: {fen}}).
         then(res => res.data, error => console.error(error));
     },
     updateBase: () => {
-      return Promise.reject('not implemented, the logic to be moved from baseUpdater');
+      const updateBase = () => {
+      const user = userService.getUser();
+      connectionIndicator.startSending();
+      $http({method: 'GET', url: backendUrl + '/api/getbase?userid=' + user.id, transformResponse: false}).
+          then(
+              function success(response){
+                  console.log("new base received, ", response.data.length, " bytes");
+                  base = JSON.parse(response.data);
+                  base.pgn = '';
+                  baseUpdated = true;
+                  baseManager.saveBase(base);
+                  connectionIndicator.success();
+              },
+              function error(response) {
+                  console.error("could not update base from server: ", response);
+                  connectionIndicator.error();
+                  setTimeout(updateBase, retryTimeout+=retryTimeout);
+              }
+          );
+      }
+      setTimeout(updateBase, 0);
     }
   };
 }])
